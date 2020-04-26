7f2ab768783d150e07e1a531019d2455011497e5
sarveshharale10 <sarveshharale10@gmail.com>
2020-04-26 22:01:34 +0530

    Basic backend ready

---
+++ backend.py
@@ -0,0 +1,255 @@
+from pymongo import MongoClient
+from flask import Flask,jsonify
+from track_trace import Tracker,Tracer
+import string
+import random
+
+'''
+var mapFunction1 = function() {
+    this.inputs.forEach(function(item){ emit(item.address,item.amount); });
+}
+
+var reduceFunction1 = function(address,amounts) {
+                          return Array.sum(amounts);
+                      };
+'''
+
+app = Flask(__name__)
+app.config["db"] = MongoClient("mongodb://localhost:27017")["ba"]
+
+def combine_inputs_outputs(responses):
+	unique_addresses = set()
+
+	for response in responses:
+
+		contributions = dict()
+		for inp in response["inputs"]:
+			try:
+				contributions[inp["address"]] += inp["amount"]
+			except:
+				contributions[inp["address"]] = inp["amount"]
+
+		inputs = list()
+		for (key,value) in contributions.items():
+			inputs.append({"address":key,"amount":value})
+			unique_addresses.add(key)
+		response["inputs"] = inputs
+	
+		contributions = dict()
+		for inp in response["outputs"]:
+			try:
+				contributions[inp["address"]] += inp["amount"]
+			except:
+				contributions[inp["address"]] = inp["amount"]
+
+		outputs = list()
+		for (key,value) in contributions.items():
+			unique_addresses.add(key)
+			outputs.append({"address":key,"amount":value})
+		response["outputs"] = outputs
+
+	return responses,unique_addresses
+
+def convert_result_to_json(query,responses):
+	graph = {}
+	graph["nodes"] = list()
+	graph["relationships"] = list()
+
+	depth_colors = {
+		0:"#87faed",
+		1:"#87cefa",
+		2:"#8795fa"
+	}
+
+	responses,unique_addresses = combine_inputs_outputs(responses)
+
+	for response in responses:
+
+		color = "#57C7E3"
+
+		if("depth" in response):
+			color = depth_colors[response["depth"]]
+
+		if(response["tx_hash"] == query):
+			color = "#ff0000"
+
+		graph["nodes"].append({
+			"id":response["tx_hash"],
+			"labels":["Transaction"],
+			"properties":{
+				"tx_hash":response["tx_hash"]
+			},
+			"color":color
+		})
+
+		for inp in response["inputs"]:
+			graph["relationships"].append({
+				"type":"INPUT",
+				"startNode":inp["address"],
+				"endNode":response["tx_hash"],
+				"properties":{
+					"amount":inp["amount"]
+				}
+			})
+
+		for inp in response["outputs"]:
+			graph["relationships"].append({
+				"type":"OUTPUT",
+				"startNode":response["tx_hash"],
+				"endNode":inp["address"],
+				"properties":{
+					"amount":inp["amount"]
+				}
+			})
+
+	for address in unique_addresses:
+		color = "#F79767"
+
+		if(address == query):
+			color = "#ff0000"
+
+		graph["nodes"].append({
+			"id":address,
+			"labels":["Address"],
+			"properties":{
+				"value":address
+			},
+			"color":color
+		})
+	
+	return jsonify(graph)
+
+def convert_result_to_cluster(responses):
+	responses,unique_addresses = combine_inputs_outputs(responses)
+
+	addresses = {}
+
+	for response in responses:
+		tag = ""
+		tag_exists = False
+		for inp in response["inputs"]:
+			address = inp["address"]
+			if(address in addresses):
+				tag = addresses[address]
+				tag_exists = True
+				break
+
+		if(tag_exists):
+			for inp in response["inputs"]:
+				address = inp["address"]
+				addresses[address] = tag
+		else:
+			tag = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
+			for inp in response["inputs"]:
+				address = inp["address"]
+				addresses[address] = tag
+
+
+		for out in response["outputs"]:
+			address = out["address"]
+			if(address not in addresses):
+				tag = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 5))
+				addresses[address] = tag
+
+	for response in responses:
+		for inp in response["inputs"]:
+			inp["address"] = addresses[inp["address"]]
+
+		for out in response["outputs"]:
+			out["address"] = addresses[out["address"]]
+
+	responses,unique_clusters = combine_inputs_outputs(responses)
+
+	return convert_result_to_json("",responses)
+
+
+@app.route("/transaction/<tx_hash>",methods=['GET'])
+def get_transaction(tx_hash):
+	db = app.config["db"]
+	transactions = db["transactions"]
+
+	result = transactions.find({"tx_hash":tx_hash})
+	responses = []
+	for row in result:
+		response = {}
+		response["tx_hash"] = row["tx_hash"]
+		response["timestamp"] = row["timestamp"]
+		response["inputs"] = row["inputs"]
+		response["outputs"] = row["outputs"]
+		responses.append(response)
+
+	return convert_result_to_json(responses)
+
+	
+
+@app.route("/address/<address>",methods=['GET'])
+def get_address(address):
+	db = app.config["db"]
+	transactions = db["transactions"]
+
+	result = transactions.find({"$or":[{"outputs":{"$elemMatch":{"address":address}}},{"inputs":{"$elemMatch":{"address":address}}}]});
+	responses = []
+	for row in result:
+		response = {}
+		response["tx_hash"] = row["tx_hash"]
+		response["timestamp"] = row["timestamp"]
+		response["inputs"] = row["inputs"]
+		response["outputs"] = row["outputs"]
+		responses.append(response)
+
+	return convert_result_to_json(responses)
+
+@app.route("/track/<address>/<hop_count>",methods=['GET'])
+def track(address,hop_count):
+	db = app.config["db"]
+	transactions = db["transactions"]
+
+	tracker = Tracker(transactions)
+
+	result = tracker.track(address,int(hop_count))
+	
+	responses = []
+	for key,value in result.items():
+		for v in value:
+			r = transactions.find({"tx_hash":v})
+			for row in r:
+				response = {}
+				response["tx_hash"] = row["tx_hash"]
+				response["timestamp"] = row["timestamp"]
+				response["inputs"] = row["inputs"]
+				response["outputs"] = row["outputs"]
+				response["depth"] = key
+				responses.append(response)
+
+	return convert_result_to_json(address,responses)
+
+@app.route("/trace/<address>/<hop_count>",methods=['GET'])
+def trace(address,hop_count):
+	db = app.config["db"]
+	transactions = db["transactions"]
+
+	tracer = Tracer(transactions)
+
+	result = tracer.trace(address,int(hop_count))
+	
+	responses = []
+	for key,value in result.items():
+		for v in value:
+			r = transactions.find({"tx_hash":v})
+			for row in r:
+				response = {}
+				response["tx_hash"] = row["tx_hash"]
+				response["timestamp"] = row["timestamp"]
+				response["inputs"] = row["inputs"]
+				response["outputs"] = row["outputs"]
+				response["depth"] = key
+				responses.append(response)
+
+	return convert_result_to_json(address,responses)
+
+@app.after_request
+def add_header(r):
+    r.headers["Access-Control-Allow-Origin"] = "*"
+    return r
+
+#transactions.find({$or:[{"outputs":{$elemMatch:{"address":"1FijBR5s3EU1JS3UokzTZbkAibgL4SXzxm"}}},{"inputs":{$elemMatch:{"address":"1FijBR5s3EU1JS3UokzTZbkAibgL4SXzxm"}}}]}).count();
---
+++ track_trace.py
@@ -0,0 +1,87 @@
+import pymongo
+import json
+
+client = pymongo.MongoClient("mongodb://localhost:27017")
+
+transactions = client["ba"]["transactions"]
+
+class Tracker():
+
+	def __init__(self,transactions):
+		self.transactions = transactions
+
+	def get_single_forward_transactions(self,address):
+
+		result = self.transactions.find({"inputs":{"$elemMatch":{"address":address}}});
+
+		single_hop_transactions = list()
+		for item in result:
+			single_hop_transactions.append(item["tx_hash"])
+
+		return single_hop_transactions
+
+	def track(self,address,hop_count):
+		result = {}
+		#returns transactions at each level
+		txs = self.get_single_forward_transactions(address)
+		result[0] = txs
+		for count in range(hop_count - 1):
+			#iterate
+			output_addresses = set()
+			for transaction in txs:
+				outputs = self.transactions.find({"tx_hash":transaction},{"_id":0,"outputs":1})[0]["outputs"]
+				for output in outputs:
+					output_addresses.add(output["address"])
+
+			txs = set()
+			for output_address in output_addresses:
+				transactions_1 = self.get_single_forward_transactions(output_address)
+				for transaction in transactions_1:
+					txs.add(transaction)
+
+			result[count + 1] = txs
+
+		return result
+
+class Tracer():
+
+	def __init__(self,transactions):
+		self.transactions = transactions
+
+	def get_single_backward_transactions(self,address):
+
+		result = self.transactions.find({"outputs":{"$elemMatch":{"address":address}}});
+
+		single_hop_transactions = list()
+		for item in result:
+			single_hop_transactions.append(item["tx_hash"])
+
+		return single_hop_transactions
+
+	def trace(self,address,hop_count):
+		result = {}
+		txs = self.get_single_backward_transactions(address)
+		result[0] = txs
+		for count in range(hop_count - 1):
+			#iterate
+			input_addresses = set()
+			for transaction in txs:
+				inputs = self.transactions.find({"tx_hash":transaction},{"_id":0,"inputs":1})[0]["inputs"]
+				for inp in inputs:
+					input_addresses.add(inp["address"])
+
+			txs = set()
+			for input_address in input_addresses:
+				transactions_1 = self.get_single_backward_transactions(input_address)
+				for transaction in transactions_1:
+					txs.add(transaction)
+
+			result[count + 1] = txs
+
+		return result
+
+tracker = Tracker(transactions)
+
+tracer = Tracer(transactions)
+
+#print(tracer.trace("1FijBR5s3EU1JS3UokzTZbkAibgL4SXzxm",2))
