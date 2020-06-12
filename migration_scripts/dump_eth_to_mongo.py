9d979a736199681fc066db363fc99aa5f48b2c4f
sarveshharale10 <sarveshharale10@gmail.com>
2020-06-12 13:13:32 +0530

    Working ethereum migration

--- migration_scripts/dump_eth_to_mongo.py
+++ migration_scripts/dump_eth_to_mongo.py
@@ -4,21 +4,14 @@ from tqdm import tqdm
 from pymongo import MongoClient
 import sys
 
-geth_ipc_path = "~/.ethereum/geth.ipc"
+geth_ipc_path = "/media/sarvesh/HD-B1/sarvesh/BlockchainAnalysis/ethereum/geth.ipc"
 w3 = Web3(Web3.IPCProvider(geth_ipc_path))
 
 executor = ProcessPoolExecutor(max_workers = int(sys.argv[1]))
 
-conn = None
+conn = MongoClient("mongodb://localhost:27017")["ethereum"]["transactions"]
 
-def create_connection():
-	global conn
-	if(conn is None):
-		conn = MongoClient("mongodb://localhost:27017")["ethereum"]["transactions"]
-	return conn
-
-def process_transaction(transaction,timestamp,conn):
-
+def process_transaction(transaction,timestamp):
 	tx_from = transaction["from"].lower()
 	tx_to = transaction["to"].lower()
 
@@ -33,18 +26,10 @@ def process_transaction(transaction,timestamp,conn):
 	doc["outputs"] = [{"address":tx_to,"amount":transaction["value"] / (10 ** 18)}]
 	conn.insert_one(doc)
 
-
-last_block = -1
-next_block = last_block + 1
-
-latest_block = w3.eth.blockNumber
-
-conn = create_connection()
 try:
 	latest_timestamp = conn.find({},{"_id":0,"timestamp":1}).sort("timestamp",-1).limit(1)[0]["timestamp"]
 	conn.remove({"timestamp":latest_timestamp})
 except Exception as e:
-	print(e)
 	latest_timestamp = 0
 
 i = 0
@@ -52,14 +37,14 @@ while(True):
 	try:
 		block = w3.eth.getBlock(i)
 		print(f"block {i} found",end="\r")
-
 		timestamp = block.timestamp
 		if(timestamp >= latest_timestamp):
 			txhashes = block.transactions
 			for txhash in txhashes:
 				transaction = dict(w3.eth.getTransaction(txhash.hex()))
-				executor.submit(process_transaction,transaction,timestamp,create_connection())
+				executor.submit(process_transaction,transaction,timestamp)
 
 		i += 1
 	except Exception as e:
+		raise e
 		continue
