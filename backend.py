23ab2867afe23b25138e4c220c8e1b36dc8a0bc3
abcf98 <acframewala_b16@ce.vjti.ac.in>
2020-06-27 23:22:45 +0530

    Back button issue fixed

--- backend.py
+++ backend.py
@@ -176,8 +176,12 @@ def logout():
 
 @app.after_request
 def add_header(r):
-    r.headers["Access-Control-Allow-Origin"] = "*"
-    return r
+	r.headers["Access-Control-Allow-Origin"] = "*"
+	r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	r.headers["Pragma"] = "no-cache"
+	r.headers["Expires"] = "0"
+	r.headers['Cache-Control'] = 'public, max-age=0'
+	return r
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
